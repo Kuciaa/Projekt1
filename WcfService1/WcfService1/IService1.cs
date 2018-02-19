c63a86bfc0fa53850a42ba3c4180bc63f0de5715
kocur8372 <kocur8372@gmail.com>
2018-02-19 23:36:51 +0100

    wcf

--- WcfService1/WcfService1/IService1.cs
+++ WcfService1/WcfService1/IService1.cs
@@ -44,32 +44,32 @@ namespace WcfService1
 
         */
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "addsiatkarz?id={ids}&imie={imie}&nazwisko={nazwisko}&wiek={wieks}&wzrost={wzrosts}&pozycja={pozycja}&klub={klub}")]
         string AddSiatkarz(string ids, string imie, string nazwisko, string wieks, string wzrosts, string pozycja, string klub);
         
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "addklub?nazwa={nazwa}&rok_zalozenia={rok_zalozenias}&trener={trener}&hala={hala}&maskotka={maskotka}")]
         string AddKlub(string nazwa, string rok_zalozenias, string trener, string hala, string maskotka);
 
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "addhala?nazwah={nazwah}&miasto={miasto}&liczba_miejsc={liczba_miejscs}")]
         string AddHala(string nazwah, string miasto, string liczba_miejscs);
 
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "editsiatkarz?id={ids}&wiek={wieks}&wzrost={wzrosts}&pozycja={pozycja}&klub={klub}")]
         void EditSiatkarz(string ids, string wieks, string wzrosts, string pozycja, string klub);
 
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "editklub?nazwa={nazwa}&trener={trener}&hala={hala}&maskotka={maskotka}")]
         void EditKlub(string nazwa, string trener, string hala, string maskotka);
 
         [OperationContract]
-        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "edithala?nazwah={nazwah}&liczba_miejsc={liczba_miejscs}")]
         void EditHala(string nazwah, string liczba_miejscs);
 
